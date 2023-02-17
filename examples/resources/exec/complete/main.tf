8f8b44d18ead5fedcfa428018abca68a59941820
Aidan Melen <aidan-melen@protonmail.com>
2023-02-16 20:34:06 -0700

    docs: creating templates for provider and resource docs.

--- examples/resources/exec/complete/main.tf
+++ examples/resources/exec/complete/main.tf
@@ -55,34 +55,3 @@ resource "snowsql_exec" "role_grant_all" {
   }
 }
 
-resource "snowsql_exec" "function" {
-  name = local.name
-
-  create {
-    statements = <<-EOT
-      CREATE OR REPLACE FUNCTION ${snowflake_database.database.name}.PUBLIC.JS_FACTORIAL(f FLOAT)
-        RETURNS FLOAT
-        LANGUAGE JAVASCRIPT
-        STRICT
-        AS '
-        if (D <= 0) {
-          return 1;
-        } else {
-          var result = 1;
-          for (var i = 2; i <= D; i++) {
-            result = result * i;
-          }
-          return result;
-        }
-        ';
-    EOT
-  }
-
-  read {
-    statements = "SHOW USER FUNCTIONS LIKE 'JS_FACTORIAL' IN DATABASE ${snowflake_database.database.name};"
-  }
-
-  delete {
-    statements = "DROP FUNCTION IF EXISTS ${snowflake_database.database.name}.PUBLIC.JS_FACTORIAL(FLOAT);"
-  }
-}
--- examples/resources/exec/complete/outputs.tf
+++ examples/resources/exec/complete/outputs.tf
@@ -1,17 +1,4 @@
-###############################################################################
-# ROLE
-###############################################################################
-
 output "role_grant_all_read_results" {
   description = "The SnowSQL query result from the read statements."
   value       = try(jsondecode(nonsensitive(snowsql_exec.role_grant_all.read_results)), null)
-}
-
-###############################################################################
-# FUNCTION
-###############################################################################
-
-output "function_read_results" {
-  description = "The SnowSQL query result from the read statements."
-  value       = try(jsondecode(nonsensitive(snowsql_exec.function.read_results)), null)
 }
